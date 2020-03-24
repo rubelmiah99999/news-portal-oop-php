5e4f99c6f72f01f602054fb20b3e02b2f026a74c
waking-eni <61460471+waking-eni@users.noreply.github.com>
2020-03-24 06:32:55 +0100

    Update userAndAdmin.php

--- database/userAndAdmin.php
+++ database/userAndAdmin.php
@@ -1,21 +1,27 @@
 <?php
 
-require_once __DIR__.'/dbFunctions.php';
+require_once __DIR__.'/dbhandler.php';
 
 class UserAndAdmin {
 
+    public function connect()
+    {
+        $db = Database::getInstance();
+        return $db;
+    }
+
     public function getUsername($table, $id) {
-        $dbFunctions = new DatabaseFunctions();
+        $db = $this->connect();
         $sql = "SELECT username FROM $table WHERE id = ? ;";
-        $result = $dbFunctions->stmtWithOneParam($sql, $id);
+        $result = $db->getWithParameter($sql, $id);
         return $result;
         exit();
     }
 
     public function getAdminNameSurname($id) {
-        $dbFunctions = new DatabaseFunctions();
+        $db = $this->connect();
         $sql = "SELECT name, surname FROM administrator WHERE id = ? ;";
-        $result = $dbFunctions->stmtWithOneParam($sql, $id);
+        $result = $db->getWithParameter($sql, $id);
         return $result;
         exit();
     }
