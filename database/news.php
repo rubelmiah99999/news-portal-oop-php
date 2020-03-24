b46c38fada2fd6a03136e5523c7f6fad1efd279e
waking-eni <61460471+waking-eni@users.noreply.github.com>
2020-03-24 06:30:39 +0100

    Update news.php

--- database/news.php
+++ database/news.php
@@ -1,65 +1,73 @@
 <?php
 
-require_once __DIR__.'/dbFunctions.php';
+require_once __DIR__.'/dbhandler.php';
 
 class News {
 
+    public function connect()
+    {
+        $db = Database::getInstance();
+        return $db;
+    }
+
     public function fetchNews($offset, $total_records_per_page) {
-        $dbFunctions = new DatabaseFunctions();
+        $db = $this->connect();
         $sql = "SELECT id, title, administrator_id, date_added, short_description, picture 
                 FROM news ORDER BY date_added DESC LIMIT $offset, $total_records_per_page ;";
-        $result = $dbFunctions->stmtWithotParam($sql);
+        $result = $db->getWithoutParameters($sql);
         return $result;
         exit();
     }
 
     public function getArticle($id) {
-        $dbFunctions = new DatabaseFunctions();
+        $db = $this->connect();
         $sql = "SELECT title, category, administrator_id, date_added, short_description, 
                 content, picture, picture_source FROM news WHERE id = ? ;";
-        $result = $dbFunctions->stmtWithOneParam($sql, $id);
+        $result = $db->getWithParameter($sql, $id);
         return $result;
         exit();
     }
 
     public function getAuthor($id) {
-        $dbFunctions = new DatabaseFunctions();
+        $db = $this->connect();
         $sql = "SELECT name, surname FROM administrator WHERE id = ? ;";
-        $result = $dbFunctions->stmtWithOneParam($sql, $id);
-        return $result;
+        $result = $db->getWithParameter($sql, $id);
+        foreach($result as $key => $author) {
+            $result2 = ''.$author['name'].' '.$author['surname'];
+        }
+        return $result2;
         exit();
     }
 
     public function getAllCategories() {
-        $dbFunctions = new DatabaseFunctions();
+        $db = $this->connect();
         $sql = "SELECT DISTINCT category FROM news ;";
-        $result = $dbFunctions->stmtWithotParam($sql);
+        $result = $db->getWithoutParameters($sql);
         return $result;
         exit();
     }
 
     public function getNewsByCategory($category) {
-        $dbFunctions = new DatabaseFunctions();
+        $db = $this->connect();
         $sql = "SELECT id, title, date_added, short_description, administrator_id, picture 
                 FROM news WHERE category = ? ;";
-        $stmt = $this->mysqli->stmt_init($this->con);
-        $result = $dbFunctions->stmtWithOneParam($sql, $category);
+        $result = $db->getWithParameter($sql, $category);
         return $result;
         exit();
     }
 
     public function getNumberOfArticles() {
-        $dbFunctions = new DatabaseFunctions();
-        $sql = "SELECT COUNT(id) as quantity FROM news ;";
-        $result = $dbFunctions->getQuantity($sql);
+        $db = $this->connect();
+        $sql = "SELECT id FROM news ;";
+        $result = $db->numRows($sql);
         return $result;
         exit();
     }
 
     public function getNumberOfCategories() {
-        $dbFunctions = new DatabaseFunctions();
-        $sql = "SELECT COUNT( DISTINCT category ) as quantity FROM news ;";
-        $result = $dbFunctions->getQuantity($sql);
+        $db = $this->connect();
+        $sql = "SELECT DISTINCT category FROM news ;";
+        $result = $db->numRows($sql);
         return $result;
         exit();
     }
