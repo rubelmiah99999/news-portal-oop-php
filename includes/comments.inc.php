d75a2cf22b8e2f23c8116eefc749c18e7031768e
waking-eni <61460471+waking-eni@users.noreply.github.com>
2020-03-25 05:16:20 +0100

    Create comments.inc.php

---
+++ includes/comments.inc.php
@@ -0,0 +1,46 @@
+<?php
+
+if (session_status() == PHP_SESSION_NONE) {
+	session_start();
+}
+
+//did we get here trough comment button
+if(isset($_POST['comment'])) {
+    require_once __DIR__.'/../database/dbhandler.php';
+    $idArticle = $_POST['idArticle'];
+    $db = Database::getInstance();
+
+    $content = str_replace(array(':', '-', '/', '*', '<', '<'), '',  $_POST['comment']);
+
+    //error handling and verifying
+    if(empty($content)) {
+        header("Location: ../public/readNews.php?error=emptycomment&id=".$idArticle);
+        exit();
+    }
+    else {
+        
+        //what is the username
+        if(isset($_SESSION['userUsername'])) {
+            $username = $_SESSION['userUsername'];
+        } else if(isset($_SESSION['administratorUsername'])) {
+            $username = $_SESSION['administratorUsername'];
+        } else {
+            $username = "Anonymous";
+        }
+
+        //inserting into database
+        //$sql = "INSERT INTO comments (username, content, news_id) VALUES (?, ?, ?) ;";
+        $table = 'comments';
+        $parameters = array($username, $content, $idArticle);
+        $db->insertData($table, $parameters);
+        header("Location: ../public/readNews.php?id=".$idArticle."&comments=success");
+		exit();
+        
+    }
+    mysqli_stmt_close($stmt);
+    mysqli_close($conn);
+
+} else {
+    header("Location: ../public/readNews.php?id=".$idArticle);
+	exit();
+}
